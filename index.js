8665b22be596f810dd86fec1cfe83561be84357e
Robin Bolscher <robinbolscher@gmail.com>
2019-12-12 16:23:03 +0100

    Adds exports for zclTypes and zclFrames (for node-zigbee)

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 const ZCLNode = require('./lib/Node');
 const Clusters = require('./lib/clusters');
+const zclTypes = require('./lib/zclTypes');
+const zclFrames = require('./lib/zclFrames');
 
 module.exports = {
     ZCLNode,
+    zclTypes,
+    zclFrames,
     ...Clusters,
 };
