a7c925958f6a309fb1529baf29340a27daec2a70
jeroenvollenbrock <jeroen@vollenbrock.nl>
2020-01-21 11:55:52 +0100

    Add client message handlers and response parsing, add test cases, fix writeAttributes

--- test/testBasicCluster.js
+++ test/testBasicCluster.js
@@ -1,5 +1,6 @@
 const Cluster = require('../lib/Cluster');
 require('../lib/clusters/basic');
+const {ZCLStandardHeader} = require('../lib/zclFrames');
 
 const Basic = Cluster.getCluster(0);
 const tst = new Basic({sendFrame: console.log});
@@ -10,3 +11,5 @@ tst.writeAttributes({
     physicalEnv: 'Corridor',
 });
 tst.factoryReset();
+
+tst.handleFrame(ZCLStandardHeader.fromBuffer(Buffer.from([0x18, 0x00, 0x0A,     0x00, 0x00, 0x20, 0x02, 0x11, 0x00, 0x30, 0x6b])))
