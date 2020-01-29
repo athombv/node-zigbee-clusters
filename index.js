7b47ffbad06ead57744a6bfdc1acba2854e27bf4
Robin Bolscher <robinbolscher@gmail.com>
2020-01-29 13:32:07 +0100

    Adds getClusterId and getClusterName utility methods

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const BoundCluster = require('./lib/BoundCluster');
 const zclTypes = require('./lib/zclTypes');
 const zclFrames = require('./lib/zclFrames');
 
+const clusterIdMap = {};
+const clusterNameMap = {};
+Object.values(Clusters).forEach((Cluster) => {
+    clusterIdMap[Cluster.NAME] = Cluster.ID;
+    clusterNameMap[Cluster.ID] = Cluster.NAME;
+});
+
+const getClusterId = (clusterName) => clusterIdMap[clusterName];
+const getClusterName = (clusterId) => clusterNameMap[clusterId];
+
 module.exports = {
+    getClusterId,
+    getClusterName,
     BoundCluster,
     ZCLNode,
     zclTypes,
