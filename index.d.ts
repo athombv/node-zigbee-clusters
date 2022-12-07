17f670901c8b7a879ff892dd7a56b00062d623b2
Robin Bolscher <robinbolscher@gmail.com>
2022-12-07 11:59:48 +0100

    fix: typedef ZCLNode

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,35 @@
 type EndpointDescriptor = {
-    endpointId: number;
-    inputClusters: number[];
-    outputClusters: number[];
+  endpointId: number;
+  inputClusters: number[];
+  outputClusters: number[];
 };
 
 type ConstructorOptions = {
-    endpointDescriptors: EndpointDescriptor[];
-    sendFrame: (endpointId: number, clusterId: number, frame: Buffer) => Promise<void>;
+  endpointDescriptors: EndpointDescriptor[];
+  sendFrame: (endpointId: number, clusterId: number, frame: Buffer) => Promise<void>;
 };
 type ZCLNodeCluster = {
-    readAttributes: (...args: string[]) => Promise<void>;
+  readAttributes: (...args: string[]) => Promise<void>;
 };
 type ZCLNodeEndpoint = {
-    clusters: { [clusterName: string]: ZCLNodeCluster };
+  clusters: { [clusterName: string]: ZCLNodeCluster };
 };
 
+interface ZCLNode {
+  endpoints: { [endpointId: number]: ZCLNodeEndpoint };
+  handleFrame: (
+    endpointId: number,
+    clusterId: number,
+    frame: Buffer,
+    meta?: unknown
+  ) => Promise<void>;
+}
+
 declare module "zigbee-clusters" {
-    export class ZCLNode {
-        constructor(options: ConstructorOptions);
-        endpoints: { [endpointId: number]: ZCLNodeEndpoint };
-        handleFrame: (
-        endpointId: number,
-        clusterId: number,
-        frame: Buffer,
-        meta?: unknown
-        ) => Promise<void>;
-    }
-    export const CLUSTER: { [key: string]: {ID: number, NAME: string, ATTRIBUTES: any, COMMANDS: any} };
+  export var ZCLNode: {
+    new (options: ConstructorOptions): ZCLNode;
+  };
+  export const CLUSTER: {
+    [key: string]: { ID: number; NAME: string; ATTRIBUTES: any; COMMANDS: any };
+  };
 }
